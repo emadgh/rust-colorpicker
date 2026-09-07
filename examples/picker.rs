9c5df1c50593a10a9160c8826c796c98cf214d17
Emad Ghasemi <emadghasemi_2006@yahoo.com>
2026-09-07 12:10:34 +0330

    chore: make picker example a clean Windows demo

--- examples/picker.rs
+++ examples/picker.rs
@@ -1,13 +1,10 @@
+#![cfg_attr(windows, windows_subsystem = "windows")]
+
 use rust_colorpicker::{Color, ColorPicker, PickerError};
 
 fn main() -> Result<(), PickerError> {
     let mut picker = ColorPicker::new();
-    let current = Color::rgb(32, 128, 224);
-
-    match picker.pick(current)? {
-        Some(selected) => println!("selected {selected}"),
-        None => println!("selection cancelled"),
-    }
-
+    let current = Color::rgb(171, 206, 179);
+    let _ = picker.pick(current)?;
     Ok(())
 }
